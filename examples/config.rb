578aab234df341823c10df4011834679d339aefe
tom <tommaso@pavese.me>
2014-08-11 23:25:04 +0100

    added more examples

--- examples/config.rb
+++ examples/config.rb
@@ -1,16 +1,25 @@
+# To optionally define and enforce a static book structure.
+# Without this, books are free-form.
+#
 ReferenceBook.define_book_structure :price, :currency, :country_name, :date_format
 
-# valid
-ReferenceBook.write_book(title: "United States") do |book|
-  book.price = 10
+
+# Given the structure defined above:
+
+
+# This is valid
+#
+ReferenceBook.write_book(title: "UnitedStates", library_key: :us) do |book|
+  book.price = 12
   book.currency = '$'
   book.country_name = 'US'
   book.date_format = '%-m/%-d/%Y'
 end
 
 
-# raises
-ReferenceBook.write_book(title: "United Kingdom") do |book|
+# This is valid too. The library_key will be ':united_kingdom'
+#
+ReferenceBook.write_book(title: "UnitedKingdom") do |book|
   book.price = 10
   book.currency = '£'
   book.country_name = 'UK'
@@ -18,10 +27,19 @@ ReferenceBook.write_book(title: "United Kingdom") do |book|
 end
 
 
-ReferenceBook.write_book(title: "Italia") do |book|
+# This will raise an error because of the missing keys
+#
+ReferenceBook.write_book(title: "Canada") do |book|
   book.price = 12
+end
+
+
+# This will raise an error because of the unexpected key
+#
+ReferenceBook.write_book(title: "Italy", library_key: :italy) do |book|
+  book.price = 10
   book.currency = '€'
   book.country_name = 'Italy'
   book.date_format = '%-d/%-m/%Y'
+  book.international_prefix = '+39'
 end
-
---
+++ examples/use.rb
@@ -0,0 +1,128 @@
+# Given this configuration:
+
+ReferenceBook.write_book(title: "Elf") do |book|
+  book.strength     = 0
+  book.dexterity    = +2
+  book.constitution = -2
+  book.intelligence = 0
+  book.wisdom       = 0
+  book.charisma     = 0
+end
+
+ReferenceBook.write_book(title: "Dwarf") do |book|
+  book.strength     = 0
+  book.dexterity    = 0
+  book.constitution = +2
+  book.intelligence = 0
+  book.wisdom       = 0
+  book.charisma     = -2
+end
+
+ReferenceBook.write_book(title: "Halfling") do |book|
+  book.strength     = -2
+  book.dexterity    = +2
+  book.constitution = 0
+  book.intelligence = 0
+  book.wisdom       = 0
+  book.charisma     = 0
+end
+
+
+
+
+# the books can be retrieved with:
+
+ReferenceBook.library.index
+# => [:elf, :dwarf, :halfling]
+
+ReferenceBook.library.elf
+ReferenceBook.library[:elf]
+
+ReferenceBook::Library.elf
+ReferenceBook::Library[:elf]
+
+# => #<struct ReferenceBook::Book::Elf
+#               strength = 0,
+#               dexterity = 2,
+#               constitution = -2,
+#               intelligence = 0,
+#               wisdom = 0,
+#               charisma = 0,
+#               title = "Elf",
+#               library_key = :elf>
+
+
+# and each book can be queried with:
+
+ReferenceBook.library.elf.dexterity
+ReferenceBook.library.elf[:dexterity]
+# => 2
+
+ReferenceBook.library.dwarf.dexterity
+ReferenceBook.library.dwarf[:dexterity]
+# => 0
+
+ReferenceBook.library[:halfling].widsom
+ReferenceBook.library[:halfling][:wisdom]
+# => 0
+
+
+
+
+
+
+# You can wrap it with your own logic:
+
+def modifier_for(race, stat)
+  race = race.downcase.to_sym
+  stat = stat.downcase.to_sym
+  ReferenceBook.library[race][stat]
+end
+
+modifier_for('Dwarf', 'Charisma')
+# => -2
+
+
+
+
+
+
+# Also, books are a thin subclass of the base Ruby Struct.
+# They are frozen, but expose the same interface:
+
+book = ReferenceBook.library.elf
+
+book.to_h
+# => {:strength => 0,
+#     :dexterity => 2,
+#     :constitution => -2,
+#     :intelligence => 0,
+#     :wisdom => 0,
+#     :charisma => 0,
+#     :title => "Elf",
+#     :library_key => :elf}
+
+book.to_a
+# => [0, 2, -2, 0, 0, 0, "Elf", :elf]
+
+book.members
+# => [:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :title, :library_key]
+
+book.select { |value| value != 0 }
+# => [2, -2, "Elf", :elf]
+
+
+book.each_pair { |k, v| puts "#{k}: #{v}" }
+# strength: 0
+# dexterity: 2
+# constitution: -2
+# intelligence: 0
+# wisdom: 0
+# charisma: 0
+# title: Elf
+# library_key: elf
+
+
+
+ReferenceBook.library.dwarf.strength = 10
+# RuntimeError: can't modify frozen ReferenceBook::Book::Dwarf
